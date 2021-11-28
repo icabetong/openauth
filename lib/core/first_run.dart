b7ea772e3da22809ea6d1f22ff9419e1c544581e
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-11-28 10:11:33 +0800

    Removed unused imports

--- lib/core/first_run.dart
+++ lib/core/first_run.dart
@@ -1,4 +1,3 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/translations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
